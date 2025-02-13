8f60c607965082adfe14ad4ce56d72d541a70620
Sarthakdevil <Sarthakrajesh2005@gmail.com>
2025-02-14 00:38:35 +0530

    changggggg

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ logger = logging.getLogger(__name__)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["https://quizbackend-dcfl.onrender.com"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
